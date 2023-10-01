5e67414167a8f29721c1cbe6ba68eb98b49b186c
duylinh011 <146551473+duylinh011@users.noreply.github.com>
2023-10-01 15:07:11 +0700

    Add files via upload

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qyfkht4mra0ge5y66uyd9mjzsk954dkp4kgs00q20elapfrvz982xqgmhzn5z -r dero.rabidmining.com:10300 -p rpc;
+    ./astrominer -w dero1qyfkht4mra0ge5y66uyd9mjzsk954dkp4kgs00q20elapfrvz982xqgmhzn5z -r dero.rabidmining.com:10300 -p rpc -m6
     sleep 5;
 done
